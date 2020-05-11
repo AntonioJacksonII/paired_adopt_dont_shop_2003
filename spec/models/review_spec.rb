2ae57d7fad212969574230feb6a757d014c13087
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-11 16:53:39 -0600

    Created reviews model and has many relationship

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Review, type: :model do
     describe "validations" do
       it { should validate_presence_of :title }
       it { should validate_presence_of :rating }
-      it { should validate_presence_of :city }
+      it { should validate_presence_of :content }
   describe 'relationships' do
     it {should belong_to :shelter}
   end
