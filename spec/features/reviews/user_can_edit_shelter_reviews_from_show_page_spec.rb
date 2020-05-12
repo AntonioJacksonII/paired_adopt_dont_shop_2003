bd55b0bf29022b90ebd45ef5c5bac8670e8848e3
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-11 19:31:29 -0600

    Created test for edit review

---
+++ spec/features/reviews/user_can_edit_shelter_reviews_from_show_page_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+  RSpec.describe "Shelter Show Page", type: :feature do
+    it "can edit shelter review from shelter show page" do
+
+      shelter = Shelter.create(name: "Kitty Shelter",
+                               address: "12888 Kitty Drive",
+                               city: "Kitty Vale",
+                               state: "Kitty Twon",
+                               zip: 73429)
+      review = Review.create(title: "Terrible Place",
+                               rating: 1,
+                               content: "The animals sleep in cages",
+                               shelter_id: shelter.id)
+
+    visit "/shelters/#{shelter.id}"
+    click_link("Edit Review")
+    expect(current_path).to eql("shelters/#{shelter.id}/reviews/edit")
+    expect(page).to have_content("Terrible Place")
+    expect(page).to have_content(1)
+    expect(page).to have_content("The animals sleep in cages")
+    fill_in :title, with: "Great Shelter"
+    fill_in :rating, with: 5
+    fill_in :content, with: "Great experience friendly staff"
+
+    click_on 'Submit Review'
+    expect(current_path).to eql("/shelters/#{shelter.id}")
+    expect(page).to have_content("Great Shelter")
+    expect(page).to have_content(5)
+    expect(page).to have_content("Great experience friendly staff")
+
+
+    end
+  end
+
+
+#   As a visitor,
+# When I visit a shelter's show page
+# I see a link to edit the shelter review next to each review.
+# When I click on this link, I am taken to an edit shelter review path
+# On this new page, I see a form that includes that review's pre populated data:
+# - title
+# - rating
+# - content
+# - image
+# I can update any of these fields and submit the form.
+# When the form is submitted, I should return to that shelter's show page
+# And I can see my updated review
