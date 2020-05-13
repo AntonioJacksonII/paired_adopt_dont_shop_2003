acff5e36acaf5f364e085e8512d4071de19c57ea
Antonio Jackson <antoniojackson@gmail.com>
2020-05-13 13:38:59 -0600

    Refactoring update action in FavoritesController

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
+  include ActionView::Helpers::TextHelper
 
   def update
     pet = Pet.find(params[:pet_id])
-    pet_id_str = pet.id.to_s
-    session[:favorites] ||= Hash.new(0)
-    session[:favorites][pet_id_str] ||= 0
-    session[:favorites][pet_id_str] = session[:favorites][pet_id_str] + 1
+    @favorites = Favorite.new(session[:favorites])
+    @favorites.add_pet(pet)
+    session[:favorites] = @favorites.contents
     flash[:notice] = "You have added #{pet.name} to your favorites!"
     redirect_to "/pets/#{pet.id}"
   end
