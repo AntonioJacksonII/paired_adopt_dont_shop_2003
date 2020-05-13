214c3dd4983b4daea50d80f35cd0f09edd88d900
Antonio Jackson <antoniojackson@gmail.com>
2020-05-13 14:56:24 -0600

    Updating Favorite model to create an array and compact and flatten at initialization, adding add_pet method to Favorite model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,10 +2,14 @@ class Favorite
   attr_reader :contents
 
   def initialize(initial_contents)
-    @contents = initial_contents || Hash.new(0)
+    @contents = [initial_contents].compact.flatten || Array.new()
   end
 
   def total_count
     @contents.count
+  end
+
+  def add_pet(pet)
+    @contents << pet
   end
 end
