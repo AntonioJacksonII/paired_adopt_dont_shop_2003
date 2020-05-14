d81f2ae80beb155d54f037a378452376716defae
Antonio Jackson <antoniojackson@gmail.com>
2020-05-14 13:58:07 -0600

    Adding has_many relationship to applications through application_pets to Pet with associated tests

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
   validates_presence_of :image, :name, :approximate_age, :sex
-
+  has_many :application_pets
+  has_many :applications, through: :application_pets
 end
