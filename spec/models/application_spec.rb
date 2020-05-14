d3cc9767d16e73c5bb08984bee4e822f3b272d90
Antonio Jackson <antoniojackson@gmail.com>
2020-05-14 13:51:04 -0600

    Adding has_many relationship with pets through application_pets to Application

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Application, type: :model do
   describe "relationships" do
     it {should have_many :application_pets}
+    it {should have_many(:pets).through(:application_pets)}
   end
 end
