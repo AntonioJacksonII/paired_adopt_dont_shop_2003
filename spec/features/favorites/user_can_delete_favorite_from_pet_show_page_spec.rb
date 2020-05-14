f4e5e71737b89c307a4c75cfa7da80ce9de13bf3
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-13 18:47:24 -0600

    Created test from unfavorite button

--- spec/features/favorites/user_can_delete_favorite_from_pet_show_page_spec.rb
+++ spec/features/favorites/user_can_delete_favorite_from_pet_show_page_spec.rb
@@ -20,5 +20,32 @@ describe "Pet Show Page", type: :feature do
       click_button("Favorite This Pet")
       expect(current_path).to eql("/pets/#{pet.id}")
       expect(page).not_to have_button("Favorite This Pet")
+
+    end
+
+    it "can see unfavorite button if pet already favorited" do
+      shelter = Shelter.create({name: "Happy Shelter",
+                               address: "12980 Grover Drive",
+                               city: "Doggy Vale",
+                               state: "Colorado",
+                               zip: 74578})
+
+      pet = Pet.create({
+        image: "Happy Url",
+        name: "Raulgh",
+        approximate_age: 27,
+        sex: "Male",
+        shelter_id: shelter.id
+        })
+
+        visit "/pets/#{pet.id}"
+        click_button("Favorite This Pet")
+        expect(current_path).to eql("/pets/#{pet.id}")
+        xpect(page).to have_content("Favorite Pets: 1")
+        click_button("Unfavorite Pet")
+        expect(current_path).to eql("/pets/#{pet.id}")
+        expect(page).to have_content("You have removed Raulgh from your favorites!")
+        expect(page).to have_content("Favorite Pets: 0")
+
     end
   end
