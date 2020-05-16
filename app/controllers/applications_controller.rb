036c35a4bf66321ae6d9c2089b87031318861900
Antonio Jackson <antoniojackson@gmail.com>
2020-05-15 19:42:31 -0600

    Adding pets helper_method to ApplicationController and updating create action in ApplicationsController to create applications for each selected pet

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,8 +4,20 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    require "pry"; binding.pry
+    pets.each do |pet|
+      if params.has_key?("#{pet.id}")
+        pet.applications.create!(application_params)
+        application_pet = session[:favorites].find{ |favorite| favorite["id"] == pet.id}
+        session[:favorites].delete(application_pet)
+      end
+    end
     flash[:notice] = "Your application was submitted!"
     redirect_to "/favorites"
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip, :phone, :description)
   end
 end
