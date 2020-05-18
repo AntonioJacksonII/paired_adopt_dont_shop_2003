a59a320ae2b4fd5dac92521556ec8ad9ebdf8cdf
Antonio Jackson <antoniojackson@gmail.com>
2020-05-18 16:57:56 -0600

    Adding additional objects to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ApplicationPet.destroy_all
+Application.destroy_all
+Pet.destroy_all
+Shelter.destroy_all
+Review.destroy_all
+
+
 shelter_1 = Shelter.create(name: "Savior's Shelter",
                            address: "11111 Saviors St.",
                            city: "Haven Central",
@@ -26,8 +33,15 @@ pet_1 = Pet.create(image: "Curly Url",
                     shelter_id: shelter_1.id,
                     description: "A Curly Pet",
                     adoption_status: "adoptable")
+pet2 = shelter_2.pets.create(image: "cute.jpg", name: "Spot", approximate_age: 2, sex: "Male", description: "Spotted Puppy!")
+pet3 = shelter_2.pets.create(image: "cat.jpg", name: "Garfield", approximate_age: 1, sex: "Male", description: "Cute cat!")
+
 
 review_1 = Review.create(title: "Terrible Place",
                          rating: 1,
                          content: "The animals sleep in cages",
                          shelter_id: shelter_1.id)
+review_2 = Review.create(title: "Wonderful Place",
+                         rating: 5,
+                         content: "The animals sleep in piles of diamonds!",
+                        shelter_id: shelter_2.id)
