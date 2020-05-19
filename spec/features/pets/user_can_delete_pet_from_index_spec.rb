3b6b39461feb2aa7adf0487c29cc83b256b3d947
Antonio Jackson <antoniojackson@gmail.com>
2020-05-19 14:36:08 -0600

    Updating test it cannot delete a pet with an approved application from the Pet Index page

--- spec/features/pets/user_can_delete_pet_from_index_spec.rb
+++ spec/features/pets/user_can_delete_pet_from_index_spec.rb
@@ -31,18 +31,27 @@ RSpec.describe "Shelter Index Page", type: :feature do
                             city: "Doggy Vale",
                             state: "Colorado",
                             zip: 74578})
-
     pet = Pet.create({
        image: "Cloudy Url",
        name: "Cloudy",
        approximate_age: 41,
        sex: "Female",
        shelter_id: shelter_1.id,
-       adoption_status: "pending"
        })
-
-     visit '/pets'
-
-    expect(page).to_not have_link("Delete Pet")
+    application1 = Application.create({
+           name: "Bob",
+           address: "222 Bob Road",
+           city: "Bob City",
+           state: "Bob State",
+           zip: "39233",
+           phone: "30332432",
+           description: "Love, pets have lots of space for them"
+         })
+      application1.pets << pet
+      visit "/applications/#{application1.id}"
+      save_and_open_page
+      click_link("Approve Pet")
+      visit '/pets'
+      expect(page).to_not have_link("Delete Pet")
   end
 end
