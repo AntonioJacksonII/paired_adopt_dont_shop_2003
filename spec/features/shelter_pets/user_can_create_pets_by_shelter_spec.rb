8e2ed744681a2031f490ce6a3978b5fe05c3e8b0
Antonio Jackson <antoniojackson@gmail.com>
2020-05-19 16:01:35 -0600

    Adding test that you cannot create a pet with an incomplete form

--- spec/features/shelter_pets/user_can_create_pets_by_shelter_spec.rb
+++ spec/features/shelter_pets/user_can_create_pets_by_shelter_spec.rb
@@ -23,4 +23,42 @@ RSpec.describe "Pet Creation Page", type: :feature do
     expect(page).to have_content("Fido")
     expect(page).to have_content("Male")
   end
+
+  it "cannot create a pet with an incomplete form" do
+    shelter = Shelter.create({name: "Happy Shelter",
+                             address: "12980 Grover Drive",
+                             city: "Doggy Vale",
+                             state: "Colorado",
+                             zip: 74578})
+
+
+    visit "/shelters/#{shelter.id}/pets"
+    click_link('Create Pet')
+    expect(current_path).to eql("/shelters/#{shelter.id}/pets/new")
+
+    fill_in :image, with: "Fido url"
+    click_button("Create Pet")
+
+    expect(current_path).to eql("/shelters/#{shelter.id}/pets/new")
+    expect(page).to have_content("Please fill in name, description, approximate age, sex")
+
+    fill_in :name, with: "Fido"
+    click_button("Create Pet")
+    expect(current_path).to eql("/shelters/#{shelter.id}/pets/new")
+    expect(page).to have_content("Please fill in description, approximate age, sex")
+
+    fill_in :description, with: "A loveable cuddley pet"
+    click_button("Create Pet")
+    expect(current_path).to eql("/shelters/#{shelter.id}/pets/new")
+    expect(page).to have_content("Please fill in approximate age, sex")
+
+    fill_in :approximate_age, with: 3
+    click_button("Create Pet")
+    expect(current_path).to eql("/shelters/#{shelter.id}/pets/new")
+    expect(page).to have_content("Please fill in sex")
+
+    fill_in :sex, with: "Male"
+    click_button("Create Pet")
+    expect(current_path).to eql("/shelters/#{shelter.id}/pets")
+  end
 end
