8f1232c92008016d0a7d88021798e4b3ce04c60d
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-19 11:06:57 -0600

    Created status models for shelter

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,4 +3,19 @@ class Shelter < ApplicationRecord
   has_many :reviews
   validates_presence_of :name, :address, :city, :state, :zip
 
+
+  def count_of_pets
+    Pet.count
+  end
+
+  def average_rating
+    reviews.average(:rating).round(1)
+  end
+
+  def number_of_applications
+
+    pets.count do |pet|
+      pet.applications
+    end
+  end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -12,4 +12,125 @@ describe Shelter, type: :model do
     it { should have_many :reviews}
   end
 
+  describe 'instance methods' do
+    it "count_of_pets" do
+    shelter = Shelter.create(name: "Kitty Shelter",
+                             address: "12888 Kitty Drive",
+                             city: "Kitty Vale",
+                             state: "Kitty Twon",
+                             zip: 73429)
+
+    expect(shelter.count_of_pets).to eql(0)
+     pet_1 = Pet.create({
+       image: "pet_url_1",
+       name: "Sparky",
+       approximate_age: 5,
+       sex: "Male",
+       shelter_id: shelter.id
+
+       })
+
+     pet_2 = Pet.create({
+       image: "pet_url_2",
+       name: "Silly",
+       approximate_age: 2,
+       sex: "Female",
+       shelter_id: shelter.id
+       })
+    expect(shelter.count_of_pets).to eql(2)
+  end
+
+  it "average_rating" do
+
+    shelter_2 = Shelter.create(name: "Doggy Shelter",
+                             address: "12888 Doggy Drive",
+                             city: "Doggy Vale",
+                             state: "Doggy Town",
+                             zip: 73429)
+
+    review_4 = Review.create(title: "Ok Shelter",
+      rating: 2,
+      content: "Not Bad",
+      picture: "Shelter Url",
+      shelter_id: shelter_2.id)
+
+    shelter = Shelter.create(name: "Kitty Shelter",
+                             address: "12888 Kitty Drive",
+                             city: "Kitty Vale",
+                             state: "Kitty Twon",
+                             zip: 73429)
+
+   review_1 = Review.create(title: "Great Shelter",
+     rating: 5,
+     content: "Awesome experience",
+     shelter_id: shelter.id)
+
+   review_2 = Review.create(title: "Worst Shelter",
+     rating: 1,
+     content: "Inhumaine Experience",
+     picture: "Shelter Url",
+     shelter_id: shelter.id)
+
+  expect(shelter.average_rating).to eql(3.0)
+
+  review_3 = Review.create(title: "Great Shelter",
+    rating: 4,
+    content: "Not a bad place, staff a little cranky",
+    picture: "Shelter Url",
+    shelter_id: shelter.id)
+
+    expect(shelter.average_rating).to eql(3.3)
+  end
+
+  it "number_of_applications" do
+    shelter = Shelter.create(name: "Kitty Shelter",
+                             address: "12888 Kitty Drive",
+                             city: "Kitty Vale",
+                             state: "Kitty Twon",
+                             zip: 73429)
+
+   pet1 = shelter.pets.create(image: "cat.jpg", name: "Garfield", approximate_age: 1, sex: "Male", description: "Cute cat!")
+   pet2 = shelter.pets.create(image: "cute.jpg", name: "Spot", approximate_age: 2, sex: "Male", description: "Spotted Puppy!")
+   pet3 = shelter.pets.create(image: "kitten.jpg", name: "Kitty", approximate_age: 2, sex: "Female", description: "Adorable kitten")
+
+   application1 = Application.create({
+       name: "Bob",
+       address: "222 Bob Road",
+       city: "Bob City",
+       state: "Bob State",
+       zip: "39233",
+       phone: "30332432",
+       description: "Love, pets have lots of space for them"
+     })
+
+   application2 = Application.create({
+     name: "Chris",
+     address: "222 Chris Road",
+     city: "Chris City",
+     state: "Chris State",
+     zip: "33233",
+     phone: "3033897987",
+     description: "Likes pets"
+   })
+
+   application3 = Application.create({
+       name: "Jim",
+       address: "222 Jim Road",
+       city: "Jim City",
+       state: "Jim State",
+       zip: "23423",
+       phone: "452343222",
+       description: "Pet lover"
+     })
+
+   application1.pets << pet1
+   application1.pets << pet2
+   application1.pets << pet3
+   application2.pets << pet1
+   application3.pets << pet1
+   expect(shelter.number_of_applications).to eql(3)
+  end
+end
+
+
 end
